d252bf6313d7e3570d8062cd6008f53888920e52
assakali <assakali.mehdi@gmail.com>
2025-01-06 10:58:05 +0100

    ajout de modification sur les critiques ainsi que la page posts et l'affichage étoiles des notes de critiques

--- litrevu/blog/views.py
+++ litrevu/blog/views.py
@@ -8,6 +8,7 @@ from .models import *
 from django.views.generic.edit import  CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.http import Http404
 
 
 
@@ -16,11 +17,10 @@ def home(request):
     form = AuthenticationForm()
     return render(request, 'blog/home.html', {'form': form})
 
+
+
+
 @login_required
-def dashboard(request):
-    return render(request, 'blog/dashboard.html')
-
-
 def dashboard(request):
     tickets = Ticket.objects.all()    
     reviews = Review.objects.all()
@@ -34,11 +34,32 @@ def dashboard(request):
         key=lambda post : post.created_at,
         reverse=True
     )
-    print("Tickets :", tickets)
-    print("Reviews :", reviews)
-    print("Posts :", posts)
-   
+
+    for post in posts:
+        if post.type == 'review':
+            post.full_stars = range(post.rating)
+            post.empty_stars = range(5 - post.rating)
+    
     return render(request, 'blog/dashboard.html', {'posts': posts})
+
+@login_required
+def user_posts(request):
+    user_tickets = Ticket.objects.filter(user=request.user).annotate(type=Value('ticket', output_field=CharField()))
+    user_reviews = Review.objects.filter(user=request.user).annotate(type=Value('review', output_field=CharField()))
+
+    user_posts = sorted(
+        chain(user_tickets, user_reviews),
+        key=lambda post : post.created_at,
+        reverse=True
+    ) 
+
+    for post in user_posts:
+        if post.type == 'review':
+            post.full_stars = range(post.rating)
+            post.empty_stars = range(5 - post.rating)
+    return render(request, 'blog/user_posts.html', {'user_posts': user_posts})
+
+        
 
 class TicketCreateView(LoginRequiredMixin, CreateView):
     model = Ticket
@@ -76,7 +97,7 @@ class ReviewWithoutTicket(CreateView):
     fields = ['title', 'description', 'image', 'review_title','rating', 'content']
 
     def form_valid(self, form):
-        form.instance.user = self.requests.user
+        form.instance.user = self.request.user
         return super().form_valid(form)
     
     def get_success_url(self):
@@ -91,17 +112,43 @@ class TicketUpdate(UpdateView):
         return Ticket.objects.filter(user=self.request.user)
     
     def get_success_url(self):
-        return reverse_lazy('dashboard')
+        return reverse_lazy('dashboard')    
+
     
-class TicketDelete(DeleteView):
-    model = Ticket
-    template_name = 'blog/ticket_delete.html'
+
+class ReviewUpdate(UpdateView):
+    model = Review
+    template_name = 'blog/review_form.html'
+    fields = ['review_title', 'content', 'rating', 'image']
 
     def get_queryset(self):
-        return Ticket.objects.filter(user=self.request.user) 
+        queryset = Review.objects.filter(user=self.request.user)
+        print("Critiques accessibles :", queryset)
+        return queryset
     
     def get_success_url(self):
         return reverse_lazy('dashboard')
+    
+
+class PostDelete(DeleteView):    
+    template_name = 'blog/post_delete.html'
+
+    def get_object(self, queryset=None):
+        model = self.kwargs.get('model')
+        pk = self.kwargs.get("pk")
+        if model == 'ticket':
+            return get_object_or_404(Ticket, pk=pk)
+        elif model == 'review':
+            return get_object_or_404(Review, pk=pk)
+        else:
+            return Http404('objet non trouvé')
+
+        
+    def get_success_url(self):
+        return reverse_lazy('dashboard')
+    
+
+
     
 
 
--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.contrib.auth.views import (
     LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
-from blog.views import home , dashboard, TicketCreateView, ReviewWithTicket, ReviewWithoutTicket, TicketUpdate, TicketDelete
+from blog.views import home , dashboard, user_posts ,TicketCreateView, ReviewWithTicket, ReviewWithoutTicket, TicketUpdate, PostDelete, ReviewUpdate
 from authentication.views import signup_page
 
 
@@ -31,11 +31,13 @@ urlpatterns = [
     path('logout', LogoutView.as_view(), name='logout'),
     path('signup/', signup_page, name='signup'),
     path('dashboard/', dashboard, name='dashboard' ),
+    path('posts/', user_posts, name='user_posts'),
     path('request-ticket/', TicketCreateView.as_view(), name = 'request_ticket'),
     path('review/ticket/<int:ticket_id>/', ReviewWithTicket.as_view(), name='review_with_ticket'),
     path('review/create/', ReviewWithoutTicket.as_view(), name='review_without_ticket'),
     path('ticket/<int:pk>/edit/', TicketUpdate.as_view(), name='ticket_edit'), 
-    path('ticket/<int:pk>/delete/', TicketDelete.as_view(), name='ticket_delete'), 
+    path('review/<int:pk>/edit/', ReviewUpdate.as_view(), name='review_edit'),
+    path('<str:model>/<int:pk>/delete/', PostDelete.as_view(), name='post_delete'), 
     
 ]
 
